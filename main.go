1592f8b225bdf23d6940fa8dd245fb517a0ca3dc
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-04 09:30:27 +1000

    Added support for overriding the port

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +12,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := ":8080"
+	if env := os.Getenv("PORT"); env != "" {
+		port = ":" + env
+	}
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
