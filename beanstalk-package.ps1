aa640a3e4fd9b21a9b8e8601a63c2bbe440ac98f
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-04 10:04:41 +1000

    Used extensions to fix file permissions

--- beanstalk-package.ps1
+++ beanstalk-package.ps1
@@ -1,4 +1,4 @@
 $env:GOOS="linux"
 $env:GOARCH="amd64"
 go build .
-Compress-Archive -Path helloworld, Procfile -DestinationPath beanstalk-app-bundle.zip -Force
+Compress-Archive -Path helloworld, Procfile, .ebextensions -DestinationPath beanstalk-app-bundle.zip -Force
