6c1ea6234bf9fb012e546cb0137bb64ce7626373
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-04 10:13:43 +1000

    Used buildfile to set permissions

---
+++ Buildfile
@@ -0,0 +1,1 @@
+setexe: chmod +x helloworld
--- beanstalk-package.ps1
+++ beanstalk-package.ps1
@@ -1,4 +1,4 @@
 $env:GOOS="linux"
 $env:GOARCH="amd64"
 go build .
-Compress-Archive -Path helloworld, Procfile, .ebextensions -DestinationPath beanstalk-app-bundle.zip -Force
+Compress-Archive -Path helloworld, Procfile, Buildfile -DestinationPath beanstalk-app-bundle.zip -Force
